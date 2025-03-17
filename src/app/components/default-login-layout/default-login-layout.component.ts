0ffb62452ed434d24395d14dd8dd1aa8f01399e5
Júlia <juliamenzss@gmail.com>
2025-03-17 10:36:03 -0300

    feat: adiciona estilos em layout default

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ButtonComponent } from '../ui/button/button.component';
 
 @Component({
   selector: 'app-default-login-layout',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './default-login-layout.component.html',
   styleUrl: './default-login-layout.component.scss'
 })
 export class DefaultLoginLayoutComponent {
+  @Input() title: string = "";
 
 }
