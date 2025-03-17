9cacc5fe06f1cf8da5cf2cae1e00bac7e50f192e
Júlia <juliamenzss@gmail.com>
2025-03-17 10:35:27 -0300

    feat: adiciona botão padrão e estilização

---
+++ src/app/components/ui/button/button.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input() text: string = "Botão";
+  @Input() type: 'primary' | 'secondary' = 'primary';
+  @Input() size: 'small' | 'large' = 'small';
+
+}
