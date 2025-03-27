1fbaad508597e36cdd6b5099b3441a4a9bb2cef0
Júlia <juliamenzss@gmail.com>
2025-03-27 15:33:14 -0300

    feat: adiciona rotas de home, shopping-list, shopping-list/create e shopping-list/:id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './features/auth/pages/login/login.component';
+import { SignupComponent } from './features/auth/pages/signup/signup.component';
+import { ShoppingListDetailComponent } from './features/shopping-list/pages/shopping-list-detail/shopping-list-detail.component';
+import { HomeComponent } from './features/home/home.component';
+import { CreateShoppingListComponent } from './features/shopping-list/pages/create-shopping-list/create-shopping-list.component';
+import { ShoppingListComponent } from './features/shopping-list/pages/shopping-list/shopping-list.component';
 
 export const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
   {
     path: "login",
     component: LoginComponent
@@ -10,5 +18,17 @@ export const routes: Routes = [
   {
     path: "signup",
     component: SignupComponent
+  },
+  {
+    path: "shopping-list",
+    component: ShoppingListComponent
+  },
+  {
+    path: "shopping-list/create",
+    component: CreateShoppingListComponent
+  },
+  {
+    path: "shopping-list/:id",
+    component: ShoppingListDetailComponent
   }
 ];
