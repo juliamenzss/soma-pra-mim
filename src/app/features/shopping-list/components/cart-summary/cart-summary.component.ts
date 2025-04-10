0730cd63f69bfb866e4e291564740165cac3bd7f
Júlia <juliamenzss@gmail.com>
2025-04-10 20:52:56 -0300

    feat: adiciona cart summary para anexar a page

--- src/app/features/shopping-list/components/cart-summary/cart-summary.component.ts
+++ src/app/features/shopping-list/components/cart-summary/cart-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cart-summary',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './cart-summary.component.scss'
 })
 export class CartSummaryComponent {
-
+@Input() totalPrice: number = 0;
+@Input() totalItems: number = 0;
 }
