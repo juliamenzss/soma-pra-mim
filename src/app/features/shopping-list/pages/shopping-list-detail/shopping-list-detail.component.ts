feaf375a865c03667451e79775ab6014a3ed1029
Júlia <juliamenzss@gmail.com>
2025-04-10 20:54:25 -0300

    feat: adiciona logica de listagem de produtos

--- src/app/features/shopping-list/pages/shopping-list-detail/shopping-list-detail.component.ts
+++ src/app/features/shopping-list/pages/shopping-list-detail/shopping-list-detail.component.ts
@@ -1,15 +1,17 @@
 import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { ListItemComponent } from '../../components/list-item/list-item.component';
 import { ShoppingListService } from '../../../../shared/services/shopping-list.service';
 import { CommonModule } from '@angular/common';
 import { ShoppingItem } from '../../../../shared/interfaces/shopping-item/shoppingItem.interface';
-import { map, Observable, switchMap, take, takeUntil } from 'rxjs';
+import { finalize, map, Observable, switchMap, take, takeUntil } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonService } from '../../../../shared/services/ui/button.service';
 import { ToastrService } from 'ngx-toastr';
 import { ShoppingItemPayload } from '../../../../shared/interfaces/shopping-item/payload-shoppingItem.interface';
 import { AddItemFormComponent } from '../../components/add-item-form/add-item-form.component';
+import { CartSummaryComponent } from '../../components/cart-summary/cart-summary.component';
 
 interface CreateItemForm {
   name: FormControl,
@@ -19,10 +21,25 @@ interface CreateItemForm {
 
 @Component({
   selector: 'app-shopping-list-detail',
-  imports: [ListItemComponent, CommonModule, ReactiveFormsModule, AddItemFormComponent],
+  imports: [ListItemComponent, CommonModule, ReactiveFormsModule, AddItemFormComponent, CartSummaryComponent],
   templateUrl: './shopping-list-detail.component.html',
   styleUrl: './shopping-list-detail.component.scss'
 })
+export class ShoppingListDetailComponent implements OnInit {
+  createItemForm!: FormGroup<CreateItemForm>
+  totalItems: number = 0;
+  totalPrice: number = 0;
+  budget: number = 0;
+  items!: ShoppingItem[];
+
+  constructor(
+    private shoppingListService: ShoppingListService,
+    public buttonService: ButtonService,
+    private toastService: ToastrService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+  ) { }
+
 export class ShoppingListDetailComponent implements OnInit {
   createItemForm!: FormGroup<CreateItemForm>
   totalItems: number = 0;
@@ -44,6 +61,11 @@ export class ShoppingListDetailComponent implements OnInit {
   ngOnInit() {
     var idRouter = this.route.snapshot.paramMap.get('id');
     this.shoppingListId = Number(idRouter);
+  shoppingListId!: number;
+
+  ngOnInit() {
+    var idRouter = this.route.snapshot.paramMap.get('id');
+    this.shoppingListId = Number(idRouter);
 
     this.items$ = this.shoppingListService.items$;
     this.getItems(this.shoppingListId);
@@ -58,6 +80,7 @@ export class ShoppingListDetailComponent implements OnInit {
 
   onItemCreated() {
     this.getListData(this.shoppingListId);
+    this.getItems(this.shoppingListId)
   }
   getListData(id: number) {
     this.shoppingListService.getShoppingList(id).pipe(take(1)).subscribe(res => {
@@ -78,39 +101,4 @@ export class ShoppingListDetailComponent implements OnInit {
       this.getListData(this.shoppingListId));
   }
 
-  handleSubmit(event: Event) {
-    event.preventDefault();
-    if (this.createItemForm.invalid) {
-      this.toastService.warning("Preencha todos os campos corretamente");
-      return;
-    }
-
-    if (this.createItemForm.valid) {
-      const payload: ShoppingItemPayload = {
-        name: this.createItemForm.value.name ?? "",
-        price: this.createItemForm.value.price ?? 0,
-        quantity: this.createItemForm.value.quantity ?? 0,
-        shoppingListId: this.shoppingListId,
-      };
-
-      const newProduct: ShoppingItem = {
-        id: 0,
-        name: payload.name,
-        price: payload.price,
-        quantity: payload.quantity,
-        shoppingListId: this.shoppingListId,
-      };
-
-      this.shoppingListService
-        .createItem(newProduct, this.shoppingListId)
-        .pipe(take(1))
-        .subscribe(() => {
-          this.createItemForm.reset();
-          setTimeout(() => {
-            this.getListData(this.shoppingListId);
-            this.getItems(this.shoppingListId);
-          }, 5000)
-        });
-    }
-  }
 }
